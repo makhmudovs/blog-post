8f42e8d1e7dc52413a92d920a1c4850b63d1f126
makhmudovs <makhmudovs50@gmail.com>
2025-02-08 21:22:23 +0800

    test functions added

---
+++ utils/list_helper.js
@@ -0,0 +1,48 @@
+const _ = require("lodash");
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.length === 0 ? 0 : _.sumBy(blogs, "likes");
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  return _.reduce(blogs, (a, b) => (a.likes > b.likes ? a : b));
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  return _.reduce(
+    blogs,
+    (a, b) =>
+      a.blogs > b.blogs
+        ? { author: a.author, blogs: a.blogs }
+        : { author: b.author, blogs: b.blogs },
+    {}
+  );
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  return _.reduce(
+    blogs,
+    (a, b) =>
+      a.likes > b.likes
+        ? { author: a.author, likes: a.likes }
+        : { author: b.author, likes: b.likes },
+    {}
+  );
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+};
